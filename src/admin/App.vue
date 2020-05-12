29e29eb6925e01cd5532eced8387f2967cbc51d0
NataliaBystrova <n.a.bystrova@gmail.com>
2020-05-12 17:34:20 +0300

    Created html for about-me admin page

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -20,6 +20,134 @@
         li.nav__item
           a.nav__link(href="#") Отзывы
 
+    section.about-me
+      .about-me__header
+        h2.about-me__title Блок "Обо мне"
+        button.about-me__button
+          span.about-me__button-icon.plus +
+          span.button__text Добавить группу
+      .about-me__body
+        ul.about-me__blocks
+          li.about-me__item
+            table
+              thead
+                tr
+                  th
+                    input.input.about-me__item-name(type="text" placeholder="Название новой группы") 
+                  th
+                  th
+                      span.about-me__control-button.about-me__control-button_save
+                  th
+                      span.about-me__control-button.about-me__control-button_close
+              tbody
+              tfoot
+                tr
+                  td
+                  td
+                    input.input.about-me__item-skill(type="text" placeholder="Новый навык") 
+                  td 
+                    input.input.about-me__item-percent(type="text" placeholder="100%") 
+                  td
+                    button.button-add
+          li.about-me__item
+            table
+              thead
+                tr
+                  th
+                    input.input.about-me__item-name(type="text" placeholder="Название новой группы" value="Workflow") 
+                  th
+                  th
+                      span.about-me__control-button.about-me__control-button_save
+                  th
+                      span.about-me__control-button.about-me__control-button_close
+              tbody
+                tr
+                  td Git
+                  td 100 %
+                  td
+                    span.about-me__control-button.about-me__control-button_edit
+                  td
+                    span.about-me__control-button.about-me__control-button_remove
+                tr
+                  td Terminal
+                  td 90 %
+                  td
+                    span.about-me__control-button.about-me__control-button_edit
+                  td
+                    span.about-me__control-button.about-me__control-button_remove
+                tr
+                  td Gulp
+                  td 80 %
+                  td
+                    span.about-me__control-button.about-me__control-button_edit
+                  td
+                    span.about-me__control-button.about-me__control-button_remove
+                tr
+                  td Webpack
+                  td 100 %
+                  td
+                    span.about-me__control-button.about-me__control-button_edit
+                  td
+                    span.about-me__control-button.about-me__control-button_remove
+              tfoot
+                tr
+                  td
+                  td
+                    input.input.about-me__item-skill(type="text" placeholder="Новый навык") 
+                  td 
+                    input.input.about-me__item-percent(type="text" placeholder="100%") 
+                  td
+                    button.button-add
+
+          li.about-me__item
+            table
+              thead
+                tr
+                  th
+                    input.input.about-me__item-name(type="text" placeholder="Название новой группы" value="Frontend") 
+                  th
+                  th
+                  th
+                      span.about-me__control-button.about-me__control-button_edit
+              tbody
+                tr
+                  td Html5
+                  td 100 %
+                  td
+                    span.about-me__control-button.about-me__control-button_edit
+                  td
+                    span.about-me__control-button.about-me__control-button_remove
+                tr
+                  td Css3
+                  td 90 %
+                  td
+                    span.about-me__control-button.about-me__control-button_save
+                  td
+                    span.about-me__control-button.about-me__control-button_close
+                tr
+                  td JavaScript
+                  td 80 %
+                  td
+                    span.about-me__control-button.about-me__control-button_edit
+                  td
+                    span.about-me__control-button.about-me__control-button_remove
+                tr
+                  td jQuery и Vue.js
+                  td 100 %
+                  td
+                    span.about-me__control-button.about-me__control-button_edit
+                  td
+                    span.about-me__control-button.about-me__control-button_remove
+              tfoot
+                tr
+                  td
+                  td
+                    input.input.about-me__item-skill(type="text" placeholder="Новый навык") 
+                  td 
+                    input.input.about-me__item-percent(type="text" placeholder="100%") 
+                  td
+                    button.button-add
+  
     section.feedback
       h2.feedback__title Блок "Отзывы"
       .feedback__feedback-form
@@ -64,51 +192,7 @@
             button.button.button_cancel.feedback-form__button(type="button") Отмена
             button.button.button_save.feedback-form__button(type='submit') Сохранить
 
-    section.about-me
-      .about-me__header
-        h2.about__title Блок "Обо мне"
-        button.button-add 
-          span.button-add__plus.plus 
-          span.button__text Добавить группу
-      .about-me__body
-        ul.about-me__blocks
-          li.about-me__item
-            .about-me__item-header
-              input.input.about-me__item-name(type="text" placeholder="Название новой группы") 
-              .about-me__item-buttons-group
-                span.about-me__button-done
-                span.about-me__button-close
-            .about-me__item-body
-              table
-            .about-me__item-footer
-              input.input.about-me__item-skill(type="text" placeholder="Новый навык")  
-              input.input.about-me__item-percent(type="text" placeholder="100%") 
-              button.button-add
-          li.about-me__item
-            .about-me__item-header
-              input.input.about-me__item-name(type="text" placeholder="Название новой группы" value="Workflow") 
-              .about-me__item-buttons-group
-                span.about-me__button-done
-                span.about-me__button-close
-            .about-me__item-body
-              table
-            .about-me__item-footer
-              input.input.about-me__item-skill(type="text" placeholder="Новый навык")  
-              input.input.about-me__item-percent(type="text" placeholder="100%") 
-              button.button-add
-          li.about-me__item
-            .about-me__item-header
-              input.input.about-me__item-name(type="text" placeholder="Название новой группы" value="Frontend") 
-              .about-me__item-buttons-group
-                span.about-me__button-done
-                span.about-me__button-close
-            .about-me__item-body
-              table
-            .about-me__item-footer
-              input.input.about-me__item-skill(type="text" placeholder="Новый навык")  
-              input.input.about-me__item-percent(type="text" placeholder="100%") 
-              button.button-add
-
+    
 
 </template>
 
@@ -116,8 +200,49 @@
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800');
   @import "../styles/mixins.pcss";
   @import "../styles/layout/base.pcss";
-  @import "../styles/blocks/forms.pcss";
+  @import "./styles/forms.pcss";
   @import "../styles/blocks/avatar.pcss";
+
+  .plus {
+    color: #fff;
+    font-size: 15px;
+    font-weight: 400;
+    display: block;
+    width: 21px;
+    height: 21px;
+    border-radius: 50%;
+    background-image: linear-gradient(to right, #006aed 0%, #3f35cb 100%);
+  }
+
+  .about-me {
+    padding: 60px;
+
+    @include tablets {
+      padding: 40px 20px;
+    }
+  }
+
+  .about-me__header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 60px;
+  }
+
+  .about-me__title {
+    margin-right: 80px;
+  }
+
+  .about-me__button {
+    color: #383bcf;
+    font-size: 16px;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+  }
+
+  .about-me__button-icon {
+    margin-right: 13px;
+  }
 
   .about-me__blocks {
     display: flex;
@@ -126,27 +251,61 @@
 
   .about-me__item {
     min-height: 387px;
+    max-width: 525px;
+    margin-right: 30px;
+    margin-bottom: 30px;
     width: 50%;
     box-shadow: 4px 3px 20px rgba(0, 0, 0, 0.07);
     padding: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    td {
+      padding: 10px 0;
+    }
+
   }
 
-  .about-me__item-header {
-    display: flex;
-    padding: 14px 0;
-    position: relative;
+  .about-me__item-name {
+    max-width: 270px;
+  }
 
-    &:after {
-      content: "";
-      display: block;
-      height: 1px;
-      width: 100%;
-      background-color: rgba(31,35,45,0.15);
-      opacity: 0.5;
-      position: absolute;
-      bottom: 0;
-      left: 0;
+  .about-me__control-button {
+    display: block;
+    width: 15px;
+    height: 12px;
+
+    &_save {
+      background: svg-load('tick.svg', fill=currentColor;, width=100%, height=100%) center center no-repeat;
+      color: green;
+      margin-right: 16px;
     }
+
+    &_close {
+      background: svg-load('cross.svg', fill=#00d70a;, width=100%, height=100%) center center no-repeat;
+    }
+
+    &_edit {
+      background: svg-load('pencil.svg', fill=#00d70a;, width=100%, height=100%) center center no-repeat;
+    }
+
+    &_remove {
+      background: svg-load('trash.svg', fill=#00d70a;, width=100%, height=100%) center center no-repeat;
+    }
+  }
+
+  .about-me__item-skill {
+    max-width: 218px;
+    margin-right: 10px;
+  }
+
+  .about-me__item-percent {
+    max-width: 74px;
   }
 
   .header {
